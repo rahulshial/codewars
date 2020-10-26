ab1b35b3bcc41b802251e76031e28f43397e9f85
Rahul <rahul.shial@gmail.com>
2020-10-26 02:34:43 +0000

    Masking a string

---
+++ maskString.js
@@ -0,0 +1,15 @@
+// return masked string
+function maskify(cc) {
+  let maskedString = "";
+  let last4 = cc.substring(cc.length - 4);
+  for(i = cc.length-4; i > 0; i--) {
+    maskedString += '#';
+  }
+  return (maskedString + last4);
+
+}
+
+console.log(maskify("1234567890"));
+console.log(maskify("4556364607935616"));
+console.log(maskify("1"));
+console.log(maskify(""));
